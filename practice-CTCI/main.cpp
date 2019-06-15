0ac1c3a3eeddb3e613925f071cec7e4c240a38d4
Dami <obayanjud@gmail.com>
2019-06-15 14:48:36 -0600

    Initial Commit

---
+++ practice-CTCI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice-CTCI
+//
+//  Created by Damilare Obayanju on 6/15/19.
+//  Copyright © 2019 Damilare Obayanju. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
